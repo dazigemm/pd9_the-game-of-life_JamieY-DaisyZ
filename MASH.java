87379d2a85f994bbba176a68e6df5892e856edb7
Andrew Fan <andrewsuicune@gmail.com>
2014-06-02 14:18:48 -0400

    Driver skeleton

---
+++ MASH.java
@@ -0,0 +1,33 @@
+//This is MASH.java, the main driver for our game.
+
+public class MASH{
+
+    //Variables
+    private int spinnernum;//Value of spinner
+    private boolean finished;
+    private int counter;//Main counter, adds each time <blah>
+    private int index;//Counter within ArrayLists. Resets when done.
+
+    //Constructors
+    public MASH(){
+	spinnernum = 0; //Will be changed in stuff
+	finished = false;
+	counter = 0;
+	index = 0;
+    }
+
+
+    //Methods
+    public void game(){//The game
+	//Player selects which game to run, STUY or LIFE.
+
+	//Stuff
+    }
+
+
+    public static void main(String[] args){
+	MASH game = new MASH();
+	
+	
+    }
+}
