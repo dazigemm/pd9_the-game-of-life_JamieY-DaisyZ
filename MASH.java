d92f45d095ac7fa48087de6f588f8235eeab7e92
Andrew Fan <andrewsuicune@gmail.com>
2014-06-02 14:23:40 -0400

    Updated MASH.java

--- MASH.java
+++ MASH.java
@@ -1,5 +1,7 @@
 //This is MASH.java, the main driver for our game.
 
+import java.util.*;
+
 public class MASH{
 
     //Variables
@@ -7,12 +9,13 @@ public class MASH{
     private boolean finished;
     private int counter;//Main counter, adds each time <blah>
     private int index;//Counter within ArrayLists. Resets when done.
+    private Random rand;
 
     //Constructors
     public MASH(){
 	spinnernum = 0; //Will be changed in stuff
 	finished = false;
-	counter = 0;
+	counter = 1;//So does't kill first item incorrectly.
 	index = 0;
     }
 
@@ -22,6 +25,12 @@ public class MASH{
 	//Player selects which game to run, STUY or LIFE.
 
 	//Stuff
+	spinner();//Changes the spinnernum, doesn't need to use the return val.
+    }
+
+    public int spinner(){
+	spinnernum = rand.nextInt(10);//Arbitrary num as of right now.
+	return spinnernum;
     }
 
 
