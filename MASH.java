aab984b21bb543ff0de0709d04f1264fb79da245
jnyip96@gmail.com <jnyip96@gmail.com>
2014-06-03 14:43:08 -0400

    June 03, 2014

--- MASH.java
+++ MASH.java
@@ -11,7 +11,14 @@ public class MASH{
     private int index;//Counter within ArrayLists. Resets when done.
     private Random rand;
 
-
+    //Categories
+    private Category MASH;
+    //Life
+    private Category Job, RetirementAge, LivingLocation, 
+	Pet, Salary, NumKids;
+    //Stuy
+    private Category GPA, Teacher, LockerFloor, 
+	LunchPeriod, College, NumFrees;
 
     //Constructors
     public MASH(){
@@ -27,12 +34,64 @@ public class MASH{
 	//Player selects which game to run, STUY or LIFE.
 	int version = gameSelect();// 0 for Stuy and 1 for Life
 	if (version == 0){// Stuy Version
-	    // Category creation
+
+	    // Creating the ArrayList of data for each Category
+	    for (int i = 0 ; i < 6; i++){
+		switch(i) {
+		case 0: ArrayList<String> GPAAL = new ArrayList<String>();
+		    break;
+		case 1: ArrayList<String> TeacherAL = new ArrayList<String>();
+		    break;
+		case 2: ArrayList<String> LockerFloorAL = new ArrayList<String>();
+		    break;
+		case 3: ArrayList<String> LunchPeriodAL = new ArrayList<String>();
+		    break;
+		case 4: ArrayList<String> CollegeAL = new ArrayList<String>();
+		    break;
+		case 5: ArrayList<String> NumFreesAL = new ArrayList<String>();
+		    break;
+		default: System.out.println ("Creating Category Error");
+		    break;
+		}
+
+		for (int j = 0; j <3; j++){
+		    
+		    Scanner scan = new Scanner (System.in);
+		    System.out.println ("Please give a");
+		    
+		}
+	    }
 	}
 	else {//Life Version
-	    //Category creation
+	    // Creating the ArrayList of data for each Category
+	    for (int i = 0 ; i < 6; i++){
+		switch(i) {
+		case 0: ArrayList<String> JobAL = new ArrayList<String>();
+		    break;
+		case 1: ArrayList<String> RetirementAgeAL = new ArrayList<String>();
+		    break;
+		case 2: ArrayList<String> LivingLocationAL = new ArrayList<String>();
+		    break;
+		case 3: ArrayList<String> PetAL = new ArrayList<String>();
+		    break;
+		case 4: ArrayList<String> SalaryAL = new ArrayList<String>();
+		    break;
+		case 5: ArrayList<String> NumKidsAL = new ArrayList<String>();
+		    break;
+		default: System.out.println ("Creating Category Error");
+		    break;
+		}
+		
+		for (int j = 0; j <3; j++){
+		    
+		    Scanner scan = new Scanner (System.in);
+		    System.out.println ("Please give a");
+		    
+		}
+	    }
 	}
-
+	}
+		
 	//Stuff
 	spinner();//Changes the spinnernum, doesn't need to use the return val.
 	//Insert While loops running the game here
