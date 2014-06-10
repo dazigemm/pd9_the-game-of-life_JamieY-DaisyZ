1e41df4d35c6f0a48abcedc0100e91962910ecc9
Andrew Fan <andrewsuicune@gmail.com>
2014-06-10 15:34:36 -0400

    Updated Statistics.java shell

--- Statistics.java
+++ Statistics.java
@@ -8,7 +8,43 @@ public class Statistics{
     private ALMaxHeap maxheap;
 
     public Statistics(){//Constructor
-	
+	minheap = new ALMinHeap();
+	maxheap = new ALMaxHeap();
     }
+
+    //Creates a String ArrayList from a column of text data. 
+    public ArrayList<String> createAListfromFile(int column){
+	return null;
+    }
+
+    //Creates an Integer ArrayList from a column of text data.
+    public ArrayList<Integer> createAListfromFile(int column){
+	return null;
+    }
+
+    /*********************************************
+	   THROWAWAY FUNCTIONS BELOW 
+    *********************************************/
+    
+    //MEAN FUNCTIONS
+    public double getMean(int[] dataM){
+	int temp = 0;
+	for(int i = 0; i<dataM.length; i++){
+	    temp+=dataM[i];
+	}
+	temp/=dataM.length;
+	return temp;
+    }
+
+    public double getMean(ArrayList<Integer> dataM){
+	int temp = 0;
+	for(int i = 0; i<dataM.size(); i++){
+	    temp+=dataM.get(i);
+	}
+	temp/=dataM.size();
+	return temp;
+    }
+
+    //MEDIAN FUNCTIONS
 
 }
