11660b09621190c43d87ed00b39484112f965de7
Andrew Fan <andrewsuicune@gmail.com>
2014-06-10 15:08:22 -0400

    Statistics file, testing fork

---
+++ Statistics.java
@@ -0,0 +1,11 @@
+//This is the Statistics.java file. It contains all the functions for statistican analysis of data.
+
+//This class is meant to be instantiated ONCE and then for its methods to be called from outside.
+
+public class Statistics{
+
+    public Statistics(){//Constructor
+
+    }
+
+}
